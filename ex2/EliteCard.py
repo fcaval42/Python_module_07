33423232a67af6df484a0b502ea5174127069716
fcaval42 <lavacfleur76@gmail.com>
2026-02-20 14:36:50 +0100

    ex2 finished; ex3 beggining

--- ex2/EliteCard.py
+++ ex2/EliteCard.py
@@ -6,37 +6,88 @@
 #  By: fcaval <fcaval@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/19 16:52:15 by fcaval          #+#    #+#               #
-#  Updated: 2026/02/19 17:03:08 by fcaval          ###   ########.fr        #
+#  Updated: 2026/02/20 14:15:03 by fcaval          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 from ex0.Card import Card
-from abc import ABC, abstractmethod
 from ex2.Combatable import Combatable
 from ex2.Magical import Magical
 
 
 class EliteCard(Card, Combatable, Magical):
-    
+
+    def __init__(self, name: str, cost: int, rarity: str, attack: int,
+                 health: int) -> None:
+        super().__init__(name, cost, rarity)
+        self.attack_power = attack
+        self.health = health
+        self.mana = 9
+
     def play(self, game_state: dict) -> dict:
-        pass
+        return {
+            "card_played": self.name
+        }
+
+# ************************************************************************* #
+#                              Combatable                                   #
+# ************************************************************************* #
 
     def attack(self, target) -> dict:
-        pass
+        try:
+            if self.mana >= self.cost:
+                info_attack = {}
+                info_attack["attacker"] = self.name
+                info_attack["target"] = target
+                info_attack["damage"] = self.attack_power
+                info_attack["combat_type"] = "turn-based combat"
+                return info_attack
+        except ValueError:
+            print("You don't have enough mana to attack")
 
     def defend(self, incoming_damage: int) -> dict:
-        pass
-
-    def cast_spell(self, spell_name: str, targets: list) -> dict:
-        pass
+        info_defend = {}
+        try:
+            if incoming_damage < self.health:
+                info_defend["defender"] = self.name
+                info_defend["damage_taken"] = self.health - incoming_damage
+                info_defend["damage_blocked"] = 0
+                info_defend["still_alive"] = True
+                return info_defend
+        except ValueError:
+            print(f"{self.name} is ko")
+            return
 
     def get_combat_stats(self) -> dict:
-        pass
+        return {
+            "attack": self.attack_power,
+            "health": self.health
+        }
 
-    def get_magic_stats(self) -> dict:
-        pass
+# ************************************************************************* #
+#                                Magical                                    #
+# ************************************************************************* #
+
+    def cast_spell(self, spell_name: str, targets: list) -> dict:
+        info_spell = {}
+        try:
+            if self.mana > 0:
+                info_spell["caster"] = self.name
+                info_spell["spell"] = spell_name
+                info_spell["targets"] = targets
+                info_spell["mana_used"] = self.cost
+                self.mana -= self.cost
+            return info_spell
+        except ValueError:
+            print("You don't have enough mana to attack")
 
     def channel_mana(self, amount: int) -> dict:
-        pass
-    
-    
+        info_mana = {}
+        info_mana["channeled"] = amount - self.cost
+        info_mana["total_mana"] = amount
+        return info_mana
+
+    def get_magic_stats(self) -> dict:
+        return {
+            "mana": self.mana
+        }
--- ex2/main.py
+++ ex2/main.py
@@ -6,13 +6,15 @@
 #  By: fcaval <fcaval@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/19 16:51:05 by fcaval          #+#    #+#               #
-#  Updated: 2026/02/19 17:20:51 by fcaval          ###   ########.fr        #
+#  Updated: 2026/02/20 14:33:25 by fcaval          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+from ex2.EliteCard import EliteCard
+
 
 def main():
-    
+
     print("\n=== DataDeck Ability System ===\n")
 
 # ************************************************************************* #
@@ -23,12 +25,34 @@ def main():
     print("- Card: ['play', 'get_card_info', 'is_playable']")
     print("- Combatable: ['attack', 'defend', 'get_combat_stats']")
     print("- Magical: ['cast_spell', 'channel_mana', 'get_magic_stats']")
+    print()
 
 # ************************************************************************* #
-#                         Playing EliteCard                                 #
+#                             Combat phase                                  #
 # ************************************************************************* #
 
-    print(f"")
+    lugia = EliteCard("Lugia_ex", 6, "Special", 180, 150)
+    ho_oh = EliteCard("Ho-oh_ex", 2, "Immersive", 80, 150)
+    airmure = EliteCard("Airmure_ex", 2, "Full-art", 70, 140)
+
+    print(f"Playing {lugia.name} (Elite Card):\n")
+    print("Combat phase:")
+    print(f"Attack result: {lugia.attack('Enemy')}")
+    print(f"Defense result: {lugia.defend(ho_oh.attack_power)}")
+    print()
+
+# ************************************************************************* #
+#                              Magic phase                                  #
+# ************************************************************************* #
+
+    print("Magic phase:")
+    tmp = lugia.cast_spell('Explosion Élémentaire', [ho_oh.name, airmure.name])
+    print(f"Spell cast: {tmp}")
+    print(f"Mana channel: {lugia.channel_mana(9)}")
+    print()
+
+    print("Multiple interface implementation successful!")
+
 
 if __name__ == "__main__":
     main()
